c3323b33c0230c7633c998ec2b66acacfad59b2d
Dmitry Kharitonov <dmitry_kharitonov@epam.com>
2022-09-14 21:47:59 +0300

    create merge conflict

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,10 +1,12 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class FirstTest {
 
     @Test
     void firstTest() {
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(new Random().nextInt() > 1);
     }
 }
